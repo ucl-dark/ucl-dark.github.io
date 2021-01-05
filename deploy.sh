598969b6ba0ec230fd6a10062e1e817a0c8a4a28
ZhengyaoJiang <jzyjiangzhengyao@gmail.com>
2021-01-05 13:37:47 +0000

    update build

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ make freeze
 git add build
 git add static
 git commit -am "update build"
-git push origin `git subtree split --prefix build origin`:gh-pages --force
+git subtree push --prefix build origin gh-pages
+#git push origin `git subtree split --prefix build origin`:gh-pages --force
