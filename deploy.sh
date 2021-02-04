6678e8eb9d00d2ee711e0e4e37c7cba4c6469beb
ZhengyaoJiang <jzyjiangzhengyao@gmail.com>
2021-02-04 14:31:36 +0000

    update build

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ echo "DARK.CS.UCL.AC.UK" > build/CNAME
 git add build
 git add static
 git commit -am "update build"
-#git subtree push --prefix build origin gh-pages
-git push origin `git subtree split --prefix build origin`:gh-pages --force
+git subtree push --prefix build origin gh-pages
+#git push origin `git subtree split --prefix build origin`:gh-pages --force
